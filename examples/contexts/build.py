774a72e93074f8a7dee35f438a52a95d9700f965
Mike Gouline <mike@gouline.net>
2019-12-11 21:19:22 +1100

    Replace molotcontexts with shell

--- examples/contexts/build.py
+++ examples/contexts/build.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-from molotcontexts import install_molot
-install_molot('0.2.2')
 
 from molot import * #pylint: disable=unused-wildcard-import
 
---
+++ examples/contexts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [[ ! -d "$DIR/.venv" ]]; then
+    echo "Creating new venv..."
+    python3 -m venv .venv \
+        && source .venv/bin/activate \
+        && pip3 install --upgrade pip \
+        && pip3 install "molot>=0.3.1"
+else
+    source .venv/bin/activate
+fi
+
+$DIR/build.py $@
